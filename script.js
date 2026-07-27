dfc63295ddd5e1fe78475a1dfa1b6f7d7ff06457
RashikIslam5 <rashikislam58373@gmail.com>
2026-07-27 15:07:02 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Rashik Islam Poetry Website
+// Version 3.0
 
+console.log("Welcome to Rashik Islam Poetry Website!");
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Website Loaded Successfully");
+});
